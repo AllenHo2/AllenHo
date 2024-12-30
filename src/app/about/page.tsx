897f607805e1d2302bb12fe7fe37c81ad5c42a71
AllenHo2 <aho22@dons.usfca.edu>
2024-12-29 22:17:06 -0800

    apostrophe issue fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,10 +9,10 @@ export default function About() {
             Hello! My name is Allen Ho. I was born and raised in the heart of San Francisco, currently also 
             pursuing a Bachelors of Science in Computer Science at the University of San Francisco.
             Excited by the new opportunities and vast fields that computer science allows to explore, I 
-            am explore different genres that coding would allow me to explore. Of the many interests and different styles that I've worked with in the past,
-            some of my favorite topics includes cybersecurity and web development. Of the two, I'm mainly focused on web development since I think that creating websites
-            allows me to show off the inner workings and designs of systems that I daydream about all the time. Some core impacts that I'd like to focus on with my projects would
-            be something related to healthcare, animal conservation, and climate change, since I care very deeply about our environment. I've also dabbled into learning more about streamlining 
+            am explore different genres that coding would allow me to explore. Of the many interests and different styles that I&apos;ve worked with in the past,
+            some of my favorite topics includes cybersecurity and web development. Of the two, I&apos;m mainly focused on web development since I think that creating websites
+            allows me to show off the inner workings and designs of systems that I daydream about all the time. Some core impacts that I&apos;d like to focus on with my projects would
+            be something related to healthcare, animal conservation, and climate change, since I care very deeply about our environment. I&apos;ve also dabbled into learning more about streamlining 
             business managements and working with linear models. 
         </p>
         <div>
